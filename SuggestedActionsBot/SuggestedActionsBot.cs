7aed6072d3f2e800a2f23bf6e2b0f29d8e1f83c1
ShuaiHua Du <shuaihua.du@outlook.com>
2023-03-08 21:03:29 +0800

    Complete the Suggested actions bot.

---
+++ SuggestedActionsBot/SuggestedActionsBot.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SuggestedActionsBot
+{
+    public class SuggestedActionsBot : ActivityHandler
+    {
+        public const string WelcomeText = "This bot will introduce you to suggestedActions. Please answer the question:";
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await SendWelcomeMessageAsync(turnContext, cancellationToken);
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var text = turnContext.Activity.Text.ToLowerInvariant();
+
+            var responseText = ProcessInput(text);
+
+            await turnContext.SendActivityAsync(responseText, cancellationToken: cancellationToken);
+
+            await SendSuggestedActionsAsync(turnContext, cancellationToken);
+        }
+
+        private static async Task SendWelcomeMessageAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in turnContext.Activity.MembersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync($"Welcom to SuggestedActionsBot", cancellationToken: cancellationToken);
+                    await SendSuggestedActionsAsync(turnContext, cancellationToken);
+                }
+            }
+        }
+
+        private static string ProcessInput(string text)
+        {
+            const string colorText = "is the best color, I agree.";
+
+            switch (text)
+            {
+                case "red":
+                    {
+                        return $"Red {colorText}";
+                    }
+
+                case "yellow":
+                    {
+                        return $"Yellow {colorText}";
+                    }
+
+                case "blue":
+                    {
+                        return $"Blue {colorText}";
+                    }
+
+                default:
+                    {
+                        return "Please select a color from the suggested action choices";
+                    }
+            }
+        }
+
+        private static async Task SendSuggestedActionsAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            var reply = MessageFactory.Text("What is your favorite color?");
+
+            reply.SuggestedActions = new SuggestedActions
+            {
+                Actions = new List<CardAction>
+                {
+                    new CardAction() { Title = "Red", Type = ActionTypes.ImBack, Value = "Red", Image = "https://via.placeholder.com/20/FF0000?text=R", ImageAltText = "R" },
+                    new CardAction() { Title = "Yellow", Type = ActionTypes.ImBack, Value = "Yellow", Image = "https://via.placeholder.com/20/FFFF00?text=Y", ImageAltText = "Y" },
+                    new CardAction() { Title = "Blue", Type = ActionTypes.ImBack, Value = "Blue", Image = "https://via.placeholder.com/20/0000FF?text=B", ImageAltText = "B" },
+                }
+            };
+
+            await turnContext.SendActivityAsync(reply, cancellationToken);
+        }
+    }
+}
