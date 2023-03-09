ae04be3db5508d7a1d96556d62f67ea6cde98547
ShuaiHua Du <shuaihua.du@outlook.com>
2023-03-09 11:03:31 +0800

    Complete the core bot with luis.

---
+++ CoreBot/CognitiveModels/FlightBookingEx.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EmptyBot v4.18.1
+
+using System.Linq;
+
+namespace CoreBot
+{
+    public partial class FlightBooking
+    {
+        public (string From, string Airport) FromEntities
+        {
+            get
+            {
+                var fromValue = Entities?._instance?.From?.FirstOrDefault()?.Text;
+                var fromAirportValue = Entities?.From?.FirstOrDefault()?.Airport?.FirstOrDefault()?.FirstOrDefault();
+                return (fromValue, fromAirportValue);
+            }
+        }
+
+        public (string To, string Airport) ToEntities
+        {
+            get
+            {
+                var toValue = Entities?._instance?.To?.FirstOrDefault()?.Text;
+                var toAirportValue = Entities?.To?.FirstOrDefault()?.Airport?.FirstOrDefault()?.FirstOrDefault();
+                return (toValue, toAirportValue);
+            }
+        }
+
+        public string TravelDate => Entities.datetime?.FirstOrDefault()?.Expressions.FirstOrDefault()?.Split('T')[0];
+    }
+}
---
+++ CoreBot/FlightBookingRecognizer.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EmptyBot v4.18.1
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Extensions.Configuration;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreBot
+{
+    public class FlightBookingRecognizer : IRecognizer
+    {
+        private readonly LuisRecognizer _recognizer;
+
+        public FlightBookingRecognizer(IConfiguration configuration)
+        {
+            var luisAppId = configuration["LuisAppId"];
+            var luisAPIKey = configuration["LuisAPIKey"];
+            var luisAPIHostName = configuration["LuisAPIHostName"];
+
+            var luisConfigured = !string.IsNullOrEmpty(luisAppId) && !string.IsNullOrEmpty(luisAPIKey) && !string.IsNullOrEmpty(luisAPIHostName);
+
+            if (luisConfigured)
+            {
+                var luisApplication = new LuisApplication(luisAppId, luisAPIKey, $"https://{luisAPIHostName}");
+
+                var recognizerOptions = new LuisRecognizerOptionsV3(luisApplication)
+                {
+                    PredictionOptions = new Microsoft.Bot.Builder.AI.LuisV3.LuisPredictionOptions
+                    {
+                        IncludeInstanceData = true
+                    }
+                };
+
+                _recognizer = new LuisRecognizer(recognizerOptions);
+            }
+        }
+
+        public virtual bool IsConfigured => _recognizer != null;
+
+        public virtual async Task<RecognizerResult> RecognizeAsync(ITurnContext turnContext, CancellationToken cancellationToken) => await _recognizer.RecognizeAsync(turnContext, cancellationToken);
+
+        public virtual async Task<T> RecognizeAsync<T>(ITurnContext turnContext, CancellationToken cancellationToken) where T : IRecognizerConvert, new() => await _recognizer.RecognizeAsync<T>(turnContext, cancellationToken);
+    }
+}
